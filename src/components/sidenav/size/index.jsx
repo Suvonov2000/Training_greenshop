1ca3384bad7c0c1b02737df018cd0b0b7c855a54
Suvonov2000 <suvonov200@gmail.com>
2025-05-05 16:57:39 +0900

    size

--- src/components/sidenav/size/index.jsx
+++ src/components/sidenav/size/index.jsx
@@ -1,4 +1,24 @@
 const Size = () => {
-  return <divc>Size</divc>;
+  return (
+    <div>
+      <h3 className="font-bold">Size</h3>
+      <div className="pl-[12px] w-full">
+        <div className="w-full flex items-center justify-between mt-[7px] hover:text-[#46A358] cursor-pointer">
+          <h3>Small</h3>
+          <h3>{12}</h3>
+        </div>
+
+        <div className="w-full flex items-center justify-between mt-[7px] hover:text-[#46A358] cursor-pointer">
+          <h3>Medium</h3>
+          <h3>{12}</h3>
+        </div>
+
+        <div className="w-full flex items-center justify-between mt-[7px] hover:text-[#46A358] cursor-pointer">
+          <h3>Large</h3>
+          <h3>{12}</h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Size;
