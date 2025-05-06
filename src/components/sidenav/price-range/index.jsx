58ece0135fef097a4c8846599ba92229ed3096fc
Suvonov2000 <suvonov200@gmail.com>
2025-05-06 12:27:00 +0900

    categories

--- src/components/sidenav/price-range/index.jsx
+++ src/components/sidenav/price-range/index.jsx
@@ -1,11 +1,19 @@
 import { Slider } from "antd";
+import { useState } from "react";
 
 const PriceRange = () => {
+  const [range, setRange] = useState([0, 1500]);
   return (
     <div>
       <h3 className="font-bold">Price Range</h3>
       <div className="pl-[12px] w-full">
-        <Slider range />
+        <Slider min={0} value={range} max={1500} onChange={setRange} range />
+        <div>
+          Price:
+          <span className="text-[#46A358] font-bold">
+            ${range[0]} - ${range[1]}
+          </span>
+        </div>
       </div>
     </div>
   );
