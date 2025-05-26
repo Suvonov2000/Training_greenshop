decd27dbe5177eea06372ce1713dbaf5984a6a28
Suvonov2000 <suvonov200@gmail.com>
2025-05-26 17:13:05 +0900

    some corrections

--- src/components/sidenav/size/index.jsx
+++ src/components/sidenav/size/index.jsx
@@ -5,7 +5,7 @@ const Size = () => {
       <div className="pl-[12px] w-full">
         <div className="w-full flex items-center justify-between mt-[7px] hover:text-[#46A358] cursor-pointer">
           <h3>Small</h3>
-          <h3>{5}</h3>
+          <h3>{10}</h3>
         </div>
 
         <div className="w-full flex items-center justify-between mt-[7px] hover:text-[#46A358] cursor-pointer">
@@ -15,7 +15,7 @@ const Size = () => {
 
         <div className="w-full flex items-center justify-between mt-[7px] hover:text-[#46A358] cursor-pointer">
           <h3>Large</h3>
-          <h3>{15}</h3>
+          <h3>{10}</h3>
         </div>
       </div>
     </div>
